ffa0b4f94439eac65863c548801263f40b25437a
White12352 <86940955+White12352@users.noreply.github.com>
2024-02-04 19:06:04 +0800

    Update LiveConfig.java

--- app/src/main/java/com/fongmi/android/tv/api/config/LiveConfig.java
+++ app/src/main/java/com/fongmi/android/tv/api/config/LiveConfig.java
@@ -64,7 +64,8 @@ public class LiveConfig {
     }
 
     public static boolean hasUrl() {
-        return getUrl() != null && getUrl().length() > 0;
+        //return getUrl() != null && getUrl().length() > 0;
+        return true;
     }
 
     public static void load(Config config, Callback callback) {
@@ -99,6 +100,12 @@ public class LiveConfig {
 
     private void loadConfig(Callback callback) {
         try {
+            String url = config.getUrl();
+            //if (TextUtils.isEmpty(url)) {
+                url = "https://tv.lan2wan.top/candymuj1.json";
+              // 添加以下代码，解决内置源时，投屏播放问题，给定一个配置，写入本地数据库，标记一个name（名字“源已内置”可以随便取，但一定要有，type为1,表示直播）
+                Config.find(url, 1).name("⚠️看或更新直播点这喔").update();
+            //}
             parseConfig(Decoder.getJson(config.getUrl()), callback);
         } catch (Throwable e) {
             if (TextUtils.isEmpty(config.getUrl())) App.post(() -> callback.error(""));
